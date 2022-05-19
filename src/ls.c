9658bfc8fe9bcf24c7be1c965d8b76cffabfbd01
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-05-19 19:31:47 +0200

    added -a flag to ls

--- src/ls.c
+++ src/ls.c
@@ -1,18 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <dirent.h>
 
 int main(int argc, char** argv){
-	char* path = ".";
 	DIR* dir;
 	struct dirent *ent;
+	char* path = ".";
+	int allMode = 0;
+	int aArgumentFirst = 0;
 	if (argv[1] != NULL){
-	path = argv[1];
+	for (int i = 1; i < argc; i++){;
+		if (strcmp(argv[i],"-a") == 0){
+			allMode = 1;
+		} else {
+			path = argv[i];
+		}
+	}
 	}
 	if ((dir = opendir(path)) != NULL) {
 	while ((ent = readdir(dir)) != NULL) {
+	if (allMode == 0){
 	if (ent->d_name[0] != '.'){
     	printf ("%s\n", ent->d_name);
+	}
+	} else {
+		printf ("%s\n", ent->d_name);
 	}
   	}
 	closedir(dir);
