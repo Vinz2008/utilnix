0ec1a9f61ba461af369d3c1211c01efd97fd4e1e
Vincent <vincentbidarddelanoe@gmail.com>
2022-07-25 15:41:32 +0200

    continued with time

--- src/time.c
+++ src/time.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
-	int link[2];
-	char output[4096];
-	pid_t pid;
-	pid = fork();
-	if (pid == 0){
 	char* argv_list[10];
 	int i;
 	int i2 = 1;
@@ -21,16 +17,31 @@ int main(int argc, char* argv[]){
 	argv_list[i++] = (char *)0;
 	//char * argv_list[] = {"ls","-lart","/home",NULL};
 	printf("argv[1] :  %s\n", argv[1]);
+	int link[2];
+	char output[4096];
+	pid_t pid;
+	if (pipe(link)==-1){
+		printf("pipe error\n");
+	}
+	if ((pid = fork()) == -1){
+		printf("fork error\n");
+	}
+	char* slash_bin = "/bin/";
+	printf("TEST\n");
+	printf("exe : %s\n", strncat(slash_bin, argv[1], strlen(argv[1])));
+
+	if (pid == 0){
 	dup2(link[1], STDOUT_FILENO);
-    	close(link[0]);
-    	close(link[1]);
-	execv(argv[1],argv_list);
+    close(link[0]);
+    close(link[1]);
+
+	execv(strncat(slash_bin, argv[1], strlen(argv[1])),argv_list);
 	//exit(0);
 	}
 	else{
-	close(link[1]);
+		close(link[1]);
     	int nbytes = read(link[0], output, sizeof(output));
-	printf("Output: (%.*s)\n", nbytes, output);
+		printf("Output: (%.*s)\n", nbytes, output);
     	wait(NULL);
 	}
 }
